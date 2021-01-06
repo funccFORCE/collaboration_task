1ed3e987a00e774a85c3b9c15cdd697f31d5d98e
Shatakshi Singh <71642167+Shatakshi-Singh@users.noreply.github.com>
2021-01-06 22:31:02 +0530

    fibonacci-shatakshi

--- program.c
+++ program.c
@@ -85,3 +85,22 @@ int evenodd_ananya()
 	return 0;
 
 }
+
+
+int fibonacci()
+	{
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i) 
+	{
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
