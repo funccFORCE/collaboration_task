018945c480b8d084640f7f3cd55536f8b3c0e440
Khushi Saxena <khushisaxena123123@gmail.com>
2021-01-06 20:11:23 +0530

    product of two no.s

--- program.c
+++ program.c
@@ -2,11 +2,15 @@
 #include<math.h>
 
 int sum(int ,int);
+int mul(int,int);
 
 int main(){
 	printf("hello funccforce members");
 	
 	printf("The sum of these numbers :%d",sum(10,10));
+	printf("The product of these numbers :%d",mul(10,10));
+	
+	
 }
 
 int sum(int a, int b)//defining function based in declaration
@@ -14,5 +18,11 @@ int sum(int a, int b)//defining function based in declaration
     int result=a+b;//find sum of two numbers
     //and result of sum stored in result variable
     return result;//returning result
+	
 }
 	
+int mul(int a, int b)
+{
+	int result =a*b;
+	return result;
+}
